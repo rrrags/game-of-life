2e1860d21eaca87718f9a4893f10dfbb05b154b0
oliver <o4ulund@gmail.com>
2025-02-09 13:07:55 +0100

    Initialized the workspace

---
+++ include/GOL.hpp
@@ -0,0 +1,8 @@
+class GOL
+{
+  public:
+    GOL(int b) : b(b) {};
+
+  private:
+    int b{0};
+};
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <memory>
+
+/*Convoy's game og life
+------------------------
+There are four rules for each cell in this game
+    1) Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    2) Any live cell with two or three live neighbours lives on to the next generation.
+    3) Any live cell with more than three live neighbours dies, as if by overpopulation.
+    4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*/
+
+int main()
+{
+    //
+
+    return 0;
+}
